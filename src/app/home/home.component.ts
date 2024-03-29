c456b62dcb860d12fd6194945074880ab53ecd7a
spshivampatel456 <spshivampatel456@gmail.com>
2024-03-29 14:33:59 +0530

    created home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { LayoutService } from '../layout/service/app.layout.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { ChartModule } from 'primeng/chart';
+import { DividerModule } from 'primeng/divider';
+import { PanelModule } from 'primeng/panel';
+import { StyleClassModule } from 'primeng/styleclass';
+import { LandingRoutingModule } from '../demo/components/landing/landing-routing.module';
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        CommonModule,
+        LandingRoutingModule,
+        DividerModule,
+        StyleClassModule,
+        ChartModule,
+        PanelModule,
+        ButtonModule,
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+    layoutService = inject(LayoutService);
+    router = inject(Router);
+}
